01134f88717687c1fee5fa4f8ad81c18bc2dce77
Gabri <gantoniazzi@goresponsa.com>
2017-06-05 14:43:55 +0200

    demo helvetia

---
+++ helvetia/Home _ Helvetia Assicurazione_files/analytics.config.js
@@ -0,0 +1,10 @@
+
+
+s.visitor = Visitor.getInstance("657A60B351E568E70A490D4D@AdobeOrg"); 
+    /* Plug-In Configuration */
+
+    s.usePlugins=true;
+    function s_doPlugins(s) {
+
+    }
+    s.doPlugins=s_doPlugins;
